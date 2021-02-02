41c8cafb5e067bbac5b5aac523f2773c630712db
xztxy <31698398+xztxy@users.noreply.github.com>
2021-02-03 00:04:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,13 +11,13 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git packages-xwrt https://github.com/x-wrt/packages.git' feeds.conf.default
-#sed -i '$a src-git luci https://github.com/x-wrt/luci.git' feeds.conf.default
-#sed -i '$a src-git routing https://github.com/x-wrt/routing.git' feeds.conf.default
-#sed -i '$a src-git telephony https://github.com/x-wrt/telephony.git' feeds.conf.default
-#sed -i '$a src-git freifunk https://github.com/x-wrt/freifunk-openwrt-packages.git' feeds.conf.default
-#sed -i '$a src-git x https://github.com/x-wrt/com.x-wrt.git' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git packages-xwrt https://github.com/x-wrt/packages.git' feeds.conf.default
+sed -i '$a src-git luci https://github.com/x-wrt/luci.git' feeds.conf.default
+sed -i '$a src-git routing https://github.com/x-wrt/routing.git' feeds.conf.default
+sed -i '$a src-git telephony https://github.com/x-wrt/telephony.git' feeds.conf.default
+sed -i '$a src-git freifunk https://github.com/x-wrt/freifunk-openwrt-packages.git' feeds.conf.default
+sed -i '$a src-git x https://github.com/x-wrt/com.x-wrt.git' feeds.conf.default
